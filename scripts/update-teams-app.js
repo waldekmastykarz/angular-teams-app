1429c9066b41d658d6ab763ca0712a0cdf7d5fc1
waldekmastykarz <waldek@mastykarz.nl>
2022-02-28 14:33:14 +0100

    Updates scripts

--- scripts/update-teams-app.js
+++ scripts/update-teams-app.js
@@ -7,45 +7,37 @@ const fs = require('fs');
 const process = require('process');
 const { execSync } = require('child_process');
 
-const defaultExternalAppId = '933b8170-ad4c-421f-b2ca-a80f2685ef08';
-const m365rcPath = path.join(__dirname, '../.m365rc.json');
-const usage = `Usage: node ${path.basename(process.argv[1])} [externalAppId]`;
+const manifestPath = path.join(__dirname, '../package/manifest.json');
+const usage = `Usage: node ${path.basename(process.argv[1])}`;
 
 run();
 
 // ---------------------------------------------------------------------------
 
 function run() {
-  const args = process.argv.slice(2);
-
-  if (args.length > 1) {
-    console.error(usage);
-    process.exit(-1);
-  }
-
-  const externalAppId = args[0] || process.env.EXTERNAL_APP_ID || defaultExternalAppId;
-  const appId = getAppId(externalAppId);
+  const teamsAppName = getTeamsAppName();
 
   try {
-    if (!appId) {
-      throw new Error('Unable to retrieve internal appId.');
+    if (!teamsAppName) {
+      throw new Error(`Unable to retrieve Teams app's name.`);
     }
 
-    console.log(`Updating Teams app with appId: ${appId}...`);
-    execSync(`m365 teams app update -i ${appId} -p angular-teams.zip`);
+    console.log(`Updating Teams app ${teamsAppName}...`);
+    execSync(`m365 teams app update --name "${teamsAppName}" --filePath angular-teams.zip`);
     console.log('Done!');
   } catch {
-    console.error('Error updating teams app!');
+    console.error('Error updating Teams app!');
     process.exit(-1);
   }
 }
 
-function getAppId(externalAppId) {
-  try {
-    console.log('Retrieving internal appId...');
-    const appId = execSync(`m365 teams app list --query "[?externalId == '${externalAppId}'] | [0].id"`).toString();
-    return JSON.parse(appId);
-  } catch {
-    return null;
+function getTeamsAppName() {
+  console.log(`Retrieving Teams app's name...`);
+  const manifestContents = fs.readFileSync(manifestPath, 'utf8');
+  if (!manifestContents) {
+    throw new Error(`Unable to read file: ${manifestPath}`);
   }
+
+  const manifest = JSON.parse(manifestContents);
+  return manifest.name?.short;
 }
